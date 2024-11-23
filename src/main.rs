b42317de5b28a424b5a1b56b0e74af963db80efd
Curly <u007d@humanenginuity.com>
2024-11-23 07:13:38 -0800

    wip: move entry point to `evos_adapter::board::boot()`

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
 #![no_std]
 #![no_main]
 #![feature(never_type)]
-#![allow(
-    clippy::future_not_send,
-    reason = "Suppress false-positives; 'future cannot be sent between threads safely' is not \
-              relevant in bare-metal environment."
-)]
 
 use defmt_rtt as _;
-use embassy_executor::Spawner;
+use embassy_executor::SendSpawner;
 use embassy_rp::{adc::{Adc, Channel, Config as AdcConfig, InterruptHandler},
                  bind_interrupts,
                  config::Config as PeriphsConfig,
@@ -18,12 +13,12 @@ use lib::error::Result;
 use panic_probe as _;
 
 #[embassy_executor::main]
-async fn main(spawner: Spawner) -> ! {
+async fn main(spawner: SendSpawner) -> ! {
     let err = inner_main(spawner).await.unwrap_err();
     panic!("{err}");
 }
 
-async fn inner_main(_spawner: Spawner) -> Result<!> {
+async fn inner_main(_spawner: SendSpawner) -> Result<!> {
     bind_interrupts!(struct Irqs {
         ADC_IRQ_FIFO => InterruptHandler;
     });
@@ -34,7 +29,7 @@ async fn inner_main(_spawner: Spawner) -> Result<!> {
     let mut accelerator = Channel::new_pin(periphs.PIN_28, Pull::None);
 
     loop {
-        let level = adc.read(&mut accelerator).await?;
+        let level = adc.read(&mut accelerator).await? >> 4;
         defmt::println!("Pin 28 level = {}", level);
         Timer::after_millis(250).await;
     }
