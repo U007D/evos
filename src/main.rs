bb67f5c7dc77fd74d5ddb9c5af85e084dae4a9ab
atro7318 <a.tro7318@gmail.com>
2024-11-23 15:07:54 -0800

    Pentiometer level changes the speed of the motor!

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,9 @@ use embassy_executor::Spawner;
 use embassy_rp::{adc::{Adc, Channel, Config as AdcConfig, InterruptHandler},
                  bind_interrupts,
                  config::Config as PeriphsConfig,
-                 gpio::Pull};
+                 gpio::Pull,
+                 pwm::{Config as PwmConfig, Pwm}};
+use embassy_rp::gpio::{Level, Output};
 use embassy_time::Timer;
 use lib::error::Result;
 use panic_probe as _;
@@ -33,9 +35,19 @@ async fn inner_main(_spawner: Spawner) -> Result<!> {
 
     let mut accelerator = Channel::new_pin(periphs.PIN_28, Pull::None);
 
+    let _motor_spin_forward = Output::new(periphs.PIN_20, Level::High);
+
+    let mut motor_pwm_config = PwmConfig::default();
+    motor_pwm_config.top = u8::MAX.into();
+    motor_pwm_config.compare_b = 0;
+    let mut motor_pwm =
+        Pwm::new_output_b(periphs.PWM_SLICE2, periphs.PIN_21, motor_pwm_config.clone());
+
     loop {
-        let level = adc.read(&mut accelerator).await?;
+        let level = adc.read(&mut accelerator).await? >> 4;
         defmt::println!("Pin 28 level = {}", level);
-        Timer::after_millis(250).await;
+        Timer::after_millis(100).await;
+        motor_pwm_config.compare_b = level;
+        motor_pwm.set_config(&motor_pwm_config);
     }
 }
